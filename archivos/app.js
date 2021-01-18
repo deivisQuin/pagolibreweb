c4ea919b949ff182ecf63e731c4e26f0d4425b25
= <=>
2021-01-18 00:47:39 -0500

    Se crea la historia

--- archivos/app.js
+++ archivos/app.js
@@ -1,6 +1,10 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
+/*inicio prueba*/
+const historia = document.querySelector("#idHistoria");
+const historiaRegresar = document.querySelector("#idHistoriaRegresar");
+/*fin de prueba*/
 
 sign_up_btn.addEventListener("click", () => {
   container.classList.add("sign-up-mode");
@@ -9,3 +13,13 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+/*eliminar inicio de prueba*/
+historia.addEventListener("click", () => {
+  container.classList.add("sign-on-mode");
+});
+
+historiaRegresar.addEventListener("click", () => {
+  container.classList.remove("sign-on-mode");
+});
+/*Eliminar fin de prueba*/
